b97639e1f6c132d536dc6a096922fa7c7189e392
gaelisaac <146019790+gaelisaac@users.noreply.github.com>
2024-03-06 09:13:21 +0300

    Add files via upload

---
+++ pay.py
@@ -0,0 +1,35 @@
+class Employee:
+    def __init__(self, emp_id, name):
+        self.emp_id = emp_id
+        self.name = name
+
+    def calculate_payroll(self):
+        raise NotImplementedError("Subclass must implement this method")
+
+
+class SalaryEmployee(Employee):
+    def __init__(self, emp_id, name, weekly_salary):
+        super().__init__(emp_id, name)
+        self.weekly_salary = weekly_salary
+
+    def calculate_payroll(self):
+        return self.weekly_salary
+
+
+class HourlyEmployee(Employee):
+    def __init__(self, emp_id, name, hours_worked, hourly_rate):
+        super().__init__(emp_id, name)
+        self.hours_worked = hours_worked
+        self.hourly_rate = hourly_rate
+
+    def calculate_payroll(self):
+        return self.hours_worked * self.hourly_rate
+
+
+class CommissionEmployee(SalaryEmployee):
+    def __init__(self, emp_id, name, weekly_salary, commission_value):
+        super().__init__(emp_id, name, weekly_salary)
+        self.commission_value = commission_value
+
+    def calculate_payroll(self):
+        return super().calculate_payroll() + self.commission_value
